dd6fac30a88f5be1701235c5700d9e76857f9fef
Aadit Mehtani <aaditmehtani@gmail.com>
2026-07-02 20:38:52 +0530

    dotenv config in index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import {app} from './app.js'
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 
+dotenv.config({
+    path: './.env'
+})
+
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 connectDB()
