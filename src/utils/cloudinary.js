c0ad3a8b593a48b72f5d2f913da06d8c8ad0d833
Aadit Mehtani <aaditmehtani@gmail.com>
2026-07-02 20:44:33 +0530

    fixed cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,11 +1,11 @@
-import {v2} from 'cloudinary' 
-import {fs} from 'fs'
+import {v2 as cloudinary} from 'cloudinary' 
+import fs from "fs"
 
 
 cloudinary.config({ 
-        cloud_name: PROCESS.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: PROCESS.env.CLOUDINARY_API_KEY, 
-        api_secret: PROCESS.env.CLOUDINARY_API_SECRET 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
