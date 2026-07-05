b99c208235c1f07f2fc899271341dd442a1f558f
Aadit Mehtani <aaditmehtani@gmail.com>
2026-07-05 14:38:29 +0530

    Added the logout logic and designed a middleware for the same purpose and also added the routes for both login and logout

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,60 @@
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { asyncHandler } from "../utils/asyncHandler"
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler(async(req, _, next) => {
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+        
+        // console.log(token);
+        if (!token) {
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    
+        if (!user) {
+            
+            throw new ApiError(401, "Invalid Access Token")
+        }
+        
+    //This is one of the most useful parts.
+    // Now every route after this middleware can do
+    // console.log(req.user)
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+    
+})
+
+
+/*
+Client
+   │
+   │
+GET /profile
+Authorization: Bearer eyJhb...
+   │
+   ▼
+verifyJWT middleware
+   │
+   ├── Read token
+   │
+   ├── Verify signature
+   │
+   ├── Decode _id
+   │
+   ├── Find user in MongoDB
+   │
+   ├── req.user = user
+   │
+   └── next()
+           │
+           ▼
+Profile Controller
+*/
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import {Router} from "express"
-import {registerUser} from "../controllers/user.controller.js"
+import {loginUser, logoutUser, registerUser} from "../controllers/user.controller.js"
 import {upload} from "../middleware/multer.middleware.js"
-
+import {verifyJWT} from "../middleware/auth.middleware.js"
 const router=Router()
 
 // control pass ab yaha kya kaam karna hai
@@ -15,5 +15,8 @@ router.route("/register").post(
     ]),
     registerUser)
 
+router.route("/login").post(loginUser)
+// middleware to verify JWT token before allowing access to the logout route
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router
